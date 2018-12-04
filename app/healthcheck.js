b458b6f1344da5e3609b5aa7d86398c5b7dfc320
taleb <taleb.benouaer@hmcts.net>
2018-12-04 15:07:00 +0000

    Fix healthcheck route

--- app/healthcheck.js
+++ app/healthcheck.js
@@ -2,26 +2,17 @@
 
 const router = require('express').Router();
 const os = require('os');
-const Healthcheck = require('app/utils/Healthcheck');
 const commonContent = require('app/resources/en/translation/common');
 const gitRevision = process.env.GIT_REVISION;
 const osHostname = os.hostname();
 
 router.get('/', (req, res) => {
-    const healthcheck = new Healthcheck();
-    healthcheck.getDownstream(healthcheck.health, healthDownstream => {
-        healthcheck.getDownstream(healthcheck.info, infoDownstream => {
-            return res.json({
-                name: commonContent.serviceName,
-                // status: healthcheck.status(healthDownstream),
-                status: 'UP',
-                uptime: process.uptime(),
-                host: osHostname,
-                version: gitRevision,
-                downstream: healthcheck.mergeInfoAndHealthData(healthDownstream,
-                    infoDownstream)
-            });
-        });
+    return res.json({
+        name: commonContent.serviceName,
+        status: 'UP',
+        uptime: process.uptime(),
+        host: osHostname,
+        version: gitRevision
     });
 });
 
