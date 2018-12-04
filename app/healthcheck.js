0e31b59de3dd96ab535fce2cabc449322ccbb3c5
taleb <taleb.benouaer@hmcts.net>
2018-12-04 16:58:20 +0000

    Fix issue with keyvault terraform config

--- app/healthcheck.js
+++ app/healthcheck.js
@@ -2,12 +2,10 @@
 
 const router = require('express').Router();
 const os = require('os');
-const commonContent = require('app/resources/en/translation/common');
 const osHostname = os.hostname();
 
 router.get('/', (req, res) => {
     return res.json({
-        name: commonContent.serviceName,
         status: 'UP'
     });
 });
